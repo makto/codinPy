7f6e3e98ca50b066cf7e9706748f0b1efb55cb01
zhongqi <zhongqi@guokr.com>
2012-08-21 15:30:31 +0800

    tmp commit

--- euler/15.py
+++ euler/15.py
@@ -2,3 +2,11 @@
 """
 Starting in the top left corner in a 20 by 20 grid, how many routes are there to the bottom right corner?
 """
+
+def num_of_choices(x, y):
+    pass
+
+# assume that the top left corner is (0, 0)
+# then the down right corner is (20, 20)
+
+for i in 
