52d12d39dd75aa77c10b63ffc638bbb65c00c0a4
zhongqi <zhongqi@guokr.com>
2012-08-21 13:42:51 +0800

    finshi 14

--- euler/14.py
+++ euler/14.py
@@ -7,3 +7,26 @@ The sequence is defined as below:
 So, if we start with 13, the sequence generated is:
     13 => 40 => 20 => 10 => 5 => 16 => 8 => 4 => 2 => 1
 """
+
+def find_len(n):
+    length = 1
+    while n != 1:
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = 3 * n + 1
+        length += 1
+    return length
+
+max_len = 1
+start_num = 1
+for i in xrange(1, 1000001):
+    length = find_len(i)
+    if length > max_len:
+        max_len = length
+        start_num = i
+
+print start_num, max_len
+
+# the starting number is 837799
+# the max length is 525
---
+++ euler/15.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+"""
+Starting in the top left corner in a 20 by 20 grid, how many routes are there to the bottom right corner?
+"""
