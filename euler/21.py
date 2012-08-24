d84fb8ff432ee7c8293653fe10c1093ff70d39d8
zhongqi <zhongqi@guokr.com>
2012-08-24 18:17:07 +0800

    finish 21, start 22

--- euler/21.py
+++ euler/21.py
@@ -3,4 +3,22 @@
 Evaluate the sum of all amicable pairs under 10000.
 """
 
+from math import sqrt
 
+def divisors(num):
+    "for positive numbers"
+    divs = set()
+    limit = int(sqrt(num) + 1)
+    for i in xrange(1, limit):
+        if num % i == 0:
+            divs.update({i, num/i})
+    return divs
+
+amicables = set()
+for num in xrange(1, 10000):
+    num_div_sum = sum(divisors(num)) - num
+    sum_div_sum = sum(divisors(num_div_sum)) - num_div_sum
+    if sum_div_sum == num and sum_div_sum < 10000 and num != num_div_sum:
+        amicables.update({num, num_div_sum})
+
+print sum(amicables)
---
+++ euler/22.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""
+What is the total of all the names scores in the file of first names?
+The file is named names.txt
+"""
+
